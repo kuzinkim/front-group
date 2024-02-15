4b9848c820e748d6737f5ebb636350234d64b665
Kim <kuzinhb2019@gmail.com>
2024-02-15 08:43:49 +0300

    adaptive brands page

--- src/scripts/components/main-page/brandsTab.js
+++ src/scripts/components/main-page/brandsTab.js
@@ -1,5 +1,8 @@
 let brandTabs = document.querySelectorAll('[data-brand-tab]')
 let brandContent = document.querySelectorAll('[data-brand-content]')
+let brandContentWrap = document.querySelector('.brands-content')
+let brandBg = document.querySelector('.brands-page__bg')
+let brandClose = document.querySelector('.brands-content__close')
 
 function removeActiveClass() {
     brandTabs.forEach(function(tab){
@@ -30,10 +33,25 @@ function brandsTabToogle() {
                     content.classList.add('is-active')
                 }
             })
+
+            if(window.matchMedia("(max-width: 1199px)").matches){
+                brandBg.classList.add('is-open')
+                brandContentWrap.classList.add('is-open')
+            }
         })
     })
 }
 
 window.addEventListener('load', function(){
     brandsTabToogle();
+
+    brandBg.addEventListener('click', function(){
+        brandBg.classList.remove('is-open')
+        brandContentWrap.classList.remove('is-open')
+    })
+
+    brandClose.addEventListener('click', function(){
+        brandBg.classList.remove('is-open')
+        brandContentWrap.classList.remove('is-open')
+    })
 })
