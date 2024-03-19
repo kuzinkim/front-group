67dc1cb39d462ec00c2ff30e521f475c8fc072d3
Kim <kuzinhb2019@gmail.com>
2024-03-19 19:59:57 +0300

    fix slider detail brand

--- src/scripts/brands-detail.js
+++ src/scripts/brands-detail.js
@@ -3,9 +3,39 @@ let brandNav = document.querySelector('.brand-nav')
 let brandBg = document.querySelector('.brand-detail__bg')
 let brandClose = document.querySelector('.brand-detail__close')
 
+const bindBrandsSlider = (node) => {
+    if (!node) {
+        return false;
+    }
+
+    const options = {
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+
+            1199: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+        }
+    }
+
+    return new Swiper(node, options);
+}
+
 window.addEventListener('DOMContentLoaded', () => {
 
-    if(window.matchMedia("(max-width: 767px)").matches){
+    for (const slider of document.querySelectorAll('.brand-detail__img .swiper')) {
+        bindBrandsSlider(slider);
+    }
+
+    if (window.matchMedia("(max-width: 767px)").matches) {
         collapseChildItems('.js-collapse-brand-text', {
             targetHeight: 150,
             buttonMore: 'Развернуть',
@@ -13,17 +43,17 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    navButton.addEventListener('click', function(){
+    navButton.addEventListener('click', function () {
         brandNav.classList.add('is-open')
         brandBg.classList.add('is-open')
     })
 
-    brandBg.addEventListener('click', function(){
+    brandBg.addEventListener('click', function () {
         brandNav.classList.remove('is-open')
         brandBg.classList.remove('is-open')
     })
 
-    brandClose.addEventListener('click', function(){
+    brandClose.addEventListener('click', function () {
         brandNav.classList.remove('is-open')
         brandBg.classList.remove('is-open')
     })
